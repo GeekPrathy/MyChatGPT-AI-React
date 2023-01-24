a00f1a06a96c1c409c3cb9a013daad7cdf5be0ae
Prathamesh Kulkarni <kprathamesh1706@gmail.com>
2023-01-24 14:54:46 +0530

    url changed

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   }, [posts])
 
   const fetchBotResponse = async() => {
-    const {data} = await axios.post("http://localhost:4000", {input},{
+    const {data} = await axios.post("https://mychatgpt-ai-react.onrender.com", {input},{
       headers: {
         "Content-Type": "application/json"
       }
